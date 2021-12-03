68dd4a7b0931f9db58cc41521482c064455147b5
insideplant <kami.10pearl@gmail.com>
2021-12-03 16:47:05 +0900

    finish typescript

--- dist/class.js
+++ dist/class.js
@@ -4,19 +4,48 @@ class Person {
         this.name = name;
         this.age = age;
         this.id = 32;
-        this.id = 31;
-        this.name = 'hehh';
-        this.id = 30;
+    }
+    static isAdult(age) {
+        if (age > 17)
+            return true;
+        return false;
     }
     incrementAge() {
         this.age += 1;
     }
     greeting() {
         console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+        this.explainJob();
     }
 }
-let person2;
-const quill = new Person('Quill', 38);
-quill.incrementAge();
-quill.greeting();
-console.log(quill.id);
+Person.species = 'Homo sapiens';
+class Teacher extends Person {
+    constructor(name, age, _subject) {
+        super(name, age);
+        this._subject = _subject;
+    }
+    explainJob() {
+        console.log(`I am a teacher and I teach ${this.subject}`);
+    }
+    get subject() {
+        if (!this._subject) {
+            throw new Error('There is no subject.');
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        if (!value) {
+            throw new Error('There is no subject.');
+        }
+        this._subject = value;
+    }
+    static getInstance() {
+        if (Teacher.instance)
+            return Teacher.instance;
+        Teacher.instance = new Teacher('Quill', 34, 'Math');
+        return Teacher.instance;
+    }
+}
+const teacher = Teacher.getInstance();
+const teacher2 = Teacher.getInstance();
+console.log(teacher, teacher2);
--- src/class.ts
+++ src/class.ts
@@ -1,10 +1,12 @@
-class Person {
+abstract class Person {
+  static species = 'Homo sapiens';
+  static isAdult(age: number) {
+    if (age > 17) return true;
+    return false;
+  }
   
   readonly id: number = 32;
-  constructor(readonly name: string, private age: number) {
-    this.id = 31;
-    this.name = 'hehh'
-    this.id = 30;
+  constructor(readonly name: string, protected age: number) {
   }
 
   incrementAge(){
@@ -13,10 +15,43 @@ class Person {
 
   greeting(this: Person){
     console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+    this.explainJob();
+  }
+  abstract explainJob(): void;
+}
+
+class Teacher extends Person {
+  private static instance: Teacher;
+
+  explainJob(){
+    console.log(`I am a teacher and I teach ${this.subject}`);
+  }
+
+  get subject(): string{
+    if(!this._subject){
+      throw new Error('There is no subject.');
+    }
+    return this._subject;
+  }
+
+  set subject(value){
+    if(!value){
+      throw new Error('There is no subject.');
+    }
+    this._subject = value;
+  }
+  
+  private constructor(name: string, age: number, private _subject: string){
+    super(name, age);
+  }
+
+  static getInstance() {
+    if(Teacher.instance) return Teacher.instance;
+    Teacher.instance = new Teacher('Quill', 34, 'Math');
+    return Teacher.instance;
   }
 }
 
-let person2: Person;
-const quill = new Person('Quill', 38);
-quill.incrementAge();
-quill.greeting();
+const teacher = Teacher.getInstance();
+const teacher2 = Teacher.getInstance();
+console.log(teacher, teacher2)
