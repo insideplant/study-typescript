18018ae937fadfd3197b404f3913e86a5ee930f2
insideplant <kami.10pearl@gmail.com>
2021-12-06 22:47:42 +0900

    lesson7

--- src/advanced.ts
+++ src/advanced.ts
@@ -21,12 +21,40 @@ type NumberBoolean = number | boolean;
 type StringNumber = string | number;
 type Mix = NumberBoolean & StringNumber;
 
-function tuUpperCase(x: string | number) {
+function toUpperCase(x: string):string;
+function toUpperCase(x: number):number;
+function toUpperCase(x: string | number): string | number {
   if(typeof x === 'string'){
-    x.toUpperCase();
+   return x.toUpperCase();
   }
-  return '';
+  return x;
 }
+
+interface TmpFunc {
+  (x: string): number;
+  (x: number): number;
+}
+
+const upperHello: TmpFunc = function (x: string | number) {return 0};
+// interface FuncA {
+//   (a: number, b: string): number;
+//   (a: string, b: number): number;
+// }
+// interface FuncB {
+//   (a: string): number;
+// }
+
+// let intersectionFunc: FuncB & FuncA;
+// intersectionFunc = function(a: number | string, b?: number | string){ return 0 }
+
+interface FuncA {
+  (a: number): number;
+}
+interface FuncB {
+  (a: string): string;
+}
+let unionFunc: FuncA | FuncB;
+unionFunc = function(a: number){return 34}
 
 type NomadWorker = Engineer | Blogger;
 function describeNomadWorkerProfile(nomadWorker: NomadWorker){
@@ -71,6 +99,67 @@ function havePet(pet: Pet) {
 havePet(new Bird());
 
 
-// const input = document.getElementById('input') as HTMLInputElement
-// input.value = 'akfjladsjfla';
+const input = document.getElementById('input')!;
 (<HTMLInputElement>document.getElementById('input')).value = 'initial'
+
+interface Designer {
+  name: string;
+  [index: string]: string;
+}
+
+const designer: Designer = {
+  name: 'Quill',
+  3: 'afa',
+
+}
+
+interface DownloadedData {
+  id: number;
+  user?: {
+    name?: {
+      first: string;
+      last: string;
+    }
+  }
+}
+
+const downloadedData: DownloadedData = {
+  id: 1
+}
+
+console.log(downloadedData.user?.name?.first);
+const userData = downloadedData.user ?? 'no-user';
+type id = DownloadedData["id"]
+
+enum Color {
+  RED,
+  BLUE
+}
+
+class AdvancedPerson {
+  name: string = 'Peter'
+  age: number = 35;
+}
+class AdvancedCar {
+  name: string = 'Prius'
+  age: number = 35;
+}
+
+let target = new AdvancedPerson();
+let source = new AdvancedCar();
+target = source;
+
+function advancedFn(...args: readonly number[]) {
+}
+
+advancedFn(0, 1)
+
+const milk = 'milk' as const; 
+let drink = milk;
+const array = [10, 20] as const;
+const peter = {
+  name: 'Peter',
+  age: 38
+} as const;
+
+type PeterType = typeof peter;
